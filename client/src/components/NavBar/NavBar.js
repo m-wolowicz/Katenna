2991cf0f5db67e9d146e3565875f1c90a96dce82
Melissa Wolowicz <mwolowicz@MWolowicz.local>
2018-04-04 09:29:01 -0400

    Layout

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -4,19 +4,16 @@ import "./NavBar.css";
 import katennaWhite from '../../img/katennaWhite.svg';
 
 const NavBar = () =>
-	<nav className="navbar navbar-expand-lg">
+	<nav className="navbar navbar-dark navbar-expand-lg">
 		<div className="container">
 			<Link className="navbar-brand" to="/">
 				<img src={katennaWhite} alt="KATENNA" id="logo" />
 			</Link>
-			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation" id="navMenuIcon">
 				<span className="navbar-toggler-icon"></span>
 			</button>
 			<div className="collapse navbar-collapse justify-content-end" id="navbarText">
 				<ul className="navbar-nav ">
-					<li className="nav-item">
-						<Link to="/">HOME</Link>
-					</li>
 					<li className="nav-item">
 						<Link to="/">FEATURES</Link>
 					</li>
